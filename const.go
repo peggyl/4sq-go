20494152716465814cf6a14d3afd00c755e3ac21
Peggy Li <peggyli.224@gmail.com>
2016-01-24 13:48:29 -0800

    add useful constants (mostly endpoints)

---
+++ const.go
@@ -0,0 +1,14 @@
+package foursquare
+
+const (
+	DefaultScheme = "https"
+
+	// Endpoints for the OAuth 2.0 flow
+	OAuthRoot     = "foursquare.com/oauth2"
+	AuthEndpoint  = "/authenticate"
+	TokenEndpoint = "/access_token"
+
+	// API endpoints
+	APIRoot    = "api.foursquare.com"
+	APIVersion = "v2"
+)
